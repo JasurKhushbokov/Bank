5a2d912f0dfe08d3edada9a873d424b1791fda24
JasurKhushbokov <khushbakovjasur@gmail.com>
2024-10-21 10:54:23 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+class CardHolder():
+    def __init__(self, cardNum, pin, firstname, lastname, balance):
+        self.cardNum = cardNum
+        self.pin = pin
+        self.firstname = firstname
+        self.lastname = lastname
+        self.balance = balance
